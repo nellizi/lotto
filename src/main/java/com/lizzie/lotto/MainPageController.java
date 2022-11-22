bbc5379184954a5d6e28de9e3eade48d94346cf8
nellizi <masterliz9095@gmail.com>
2022-11-23 02:13:53 +0900

    Feat : api 추가

--- src/main/java/com/lizzie/lotto/MainPageController.java
+++ src/main/java/com/lizzie/lotto/MainPageController.java
@@ -5,13 +5,46 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Controller
 public class MainPageController {
 
-    @ResponseBody
     @GetMapping("/main")
-    public String mainpage(){
-        return "메인페이지";
+    public String mainpage() {
+
+        return "/main";
     }
+
+
+    @ResponseBody
+    @GetMapping("/api")
+    public StringBuffer api() throws IOException {
+        URL url = new URL("https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=861");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Content-type", "application/json");
+        conn.setDoOutput(true);
+
+
+        StringBuffer sb = null;
+        try {
+            sb = new StringBuffer();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+            while (br.ready()) {
+                sb.append(br.readLine());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sb;
+    }
+
 
 }
